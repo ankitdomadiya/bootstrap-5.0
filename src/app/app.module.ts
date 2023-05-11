52beda633c08138813bfecd2afc42173b3739e3d
ankit domadiya <ankitdomadiya6@gmail.com>
2023-05-11 11:15:41 +0530

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccordionComponent
+    AccordionComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
